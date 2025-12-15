7444c80e3424577bec0cc84fc7e7cc943c2867da
Example User <user@example.com>
2025-12-15 13:08:26 +0200

    fix: pass cleaning password to both markRoomAsClean and markRoomAsDirty in Cleaning.js

--- client/src/pages/Cleaning.js
+++ client/src/pages/Cleaning.js
@@ -128,7 +128,7 @@ const Cleaning = () => {
   const markRoomAsClean = async (taskId) => {
     setLoading(true);
     try {
-      await cleaningService.markRoomAsClean(taskId);
+      await cleaningService.markRoomAsClean(taskId, cleaningPassword);
       
       // עדכון המצב המקומי
       setCleaningTasks(prevTasks => {
