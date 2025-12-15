487d4cc112a63ebceb728e656a6c07f400b2545f
Example User <user@example.com>
2025-12-15 13:14:27 +0200

    fix: add x-cleaning-password header to CORS allowed headers

--- server/server.js
+++ server/server.js
@@ -78,7 +78,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'x-cleaning-password'],
   exposedHeaders: ['Content-Length', 'Authorization'],
   optionsSuccessStatus: 204,
   preflightContinue: false
